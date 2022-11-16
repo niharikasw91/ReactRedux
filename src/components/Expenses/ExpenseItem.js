234b002faa2e95d42fbc5550d7bbf4d458c31d99
niharikasweta <niharikasw91@gmail.com>
2022-11-16 16:05:49 -0500

    Fix styles

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,18 @@
 import "./ExpenseItem.css";
+import "./ExpenseItem.css";
 import "./ExpenseDate";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
-      <div className="expense-item__description">
+      <div className='expense-item__description'>
         <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">${props.amount}</div>
-    </div>
+      <div className='expense-item__price'>${props.amount}</div>
+    </Card>
   );
 }
 export default ExpenseItem;
